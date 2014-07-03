b57772aa6de585664ef1773c3ab93fd2c2f6bcab
AYUSH GUPTA <ayushguptadtu@gmail.com>
2014-07-03 23:32:06 +0530

    intermediate visual added - bar graph for student

--- mods/_standard/tracker/my_stats.php
+++ mods/_standard/tracker/my_stats.php
@@ -16,6 +16,7 @@ define('AT_INCLUDE_PATH', '../../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 require(AT_INCLUDE_PATH.'header.inc.php');
 ?>
+
 <table class="data static" summary="">
 <thead>
 <tr>
@@ -34,6 +35,14 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 	        FROM %smember_track 
 	        WHERE course_id=%d AND member_id=%d GROUP BY content_id ORDER BY total_hits DESC";
 	$rows_hits = queryDB($sql, array(TABLE_PREFIX, $_SESSION['course_id'], $_SESSION['member_id']));
+
+	$sql = "SELECT a.year,a.Avg_time, b.Your_avg_time FROM
+(SELECT `tool_name` as year, SUM(`duration`)/SUM(`counter`) as Avg_time FROM at_tool_track GROUP BY `tool_name`)a 
+JOIN (SELECT `tool_name` as year, SUM(`duration`)/SUM(`counter`) as Your_avg_time FROM at_tool_track WHERE `member_id` = 2 GROUP BY `tool_name`)b
+ON a.year = b.year";
+	$rows_hits1 = queryDB($sql);
+	
+ echo json_encode($rows_hits1);
     if(count($rows_hits) > 0){
 	    foreach($rows_hits as $row){
 			if ($row['total'] == '') {
@@ -65,4 +74,121 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 	?>
 </tbody>
 </table>
+
+<span id = "bargraph"> 
+
+</span>
+
 <?php require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
+
+body {
+  font: 10px sans-serif;
+}
+
+.y.axisRight text {
+    fill: orange;
+}
+
+.y.axisLeft text {
+    fill: steelblue;
+}
+
+.axis path,
+.axis line {
+  fill: none;
+  stroke: #000;
+  shape-rendering: crispEdges;
+}
+
+.bar1 {
+  fill: steelblue;
+}
+
+.bar2 {
+  fill: orange;
+}
+
+.x.axis path {
+  display: none;
+}
+
+</style>
+<body>
+<script src="http://d3js.org/d3.v3.min.js"></script>
+<script>
+
+var margin = {top: 80, right: 80, bottom: 80, left: 80},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y0 = d3.scale.linear().domain([0, 80]).range([height, 0]),
+y1 = d3.scale.linear().domain([0, 80]).range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+// create left yAxis
+var yAxisLeft = d3.svg.axis().scale(y0).ticks(10).orient("left");
+// create right yAxis
+var yAxisRight = d3.svg.axis().scale(y1).ticks(10).orient("right");
+
+var svg = d3.select("#bargraph").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("class", "graph")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var data = <?php echo json_encode($rows_hits1); ?>;
+console.log(data[0]);
+  x.domain(data.map(function(d) { return d.year; }));
+  
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+	  .attr("class", "y axis axisLeft")
+	  .attr("transform", "translate(0,0)")
+	  .call(yAxisLeft)
+	.append("text")
+	  .attr("y", 20)
+	  .attr("dy", "-2em")
+	  .style("text-anchor", "end")
+	  .text("Avg_time");
+	
+  svg.append("g")
+	  .attr("class", "y axis axisRight")
+	  .attr("transform", "translate(" + (width) + ",0)")
+	  .call(yAxisRight)
+	.append("text")
+	  .attr("y", 20)
+	  .attr("dy", "-2em")
+	  .attr("dx", "2em")
+	  .style("text-anchor", "end")
+	  .text("Your_Avg_time");
+
+  bars = svg.selectAll(".bar").data(data).enter();
+
+  bars.append("rect")
+      .attr("class", "bar1")
+      .attr("x", function(d) { return x(d.year); })
+      .attr("width", x.rangeBand()/2)
+      .attr("y", function(d) { return y0(d.Avg_time); })
+	  .attr("height", function(d,i,j) { return height - y0(d.Avg_time); }); 
+
+  bars.append("rect")
+      .attr("class", "bar2")
+      .attr("x", function(d) { return x(d.year) + x.rangeBand()/2; })
+      .attr("width", x.rangeBand() / 2)
+      .attr("y", function(d) { return y1(d.Your_avg_time); })
+	  .attr("height", function(d,i,j) { return height - y1(d.Your_avg_time); }); 
+
+</script>
