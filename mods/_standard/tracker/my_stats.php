ab953d79cd7c6688519460fd20b1cf81e2623002
AYUSH GUPTA <ayushguptadtu@gmail.com>
2014-07-08 09:24:52 +0530

    spearated content details

--- mods/_standard/tracker/my_stats.php
+++ mods/_standard/tracker/my_stats.php
@@ -18,65 +18,7 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 require('./css/student_bar_graph.css');
 
 ?>
-<head>
-<link rel="stylesheet" href="CSS/student_bar_graph.css" type="text/css">
-</head>
-<body>
-<table class="data static" summary="">
-<thead>
-<tr>
-	<th scope="col"><?php echo _AT('page'); ?></th>
-	<th scope="col"><?php echo _AT('visits'); ?></th>
-	<th scope="col"><?php echo _AT('duration'); ?></th>
-	<th scope="col"><?php echo _AT('last_accessed'); ?></th>
-</tr>
-</thead>
-<tbody>
-<?php
-	$sql = "SELECT content_id, COUNT(*) AS unique_hits, 
-	            SUM(counter) AS total_hits, 
-	            SEC_TO_TIME(SUM(duration)/SUM(counter)) AS average_duration, 
-	            SEC_TO_TIME(SUM(duration)) AS total_duration, last_accessed 
-	        FROM %smember_track 
-	        WHERE course_id=%d AND member_id=%d GROUP BY content_id ORDER BY total_hits DESC";
-	$rows_hits = queryDB($sql, array(TABLE_PREFIX, $_SESSION['course_id'], $_SESSION['member_id']));
-
-    if(count($rows_hits) > 0){
-	    foreach($rows_hits as $row){
-			if ($row['total'] == '') {
-				$row['total'] = _AT('na');
-			}
-
-			echo '<tr>';
-			echo '<td><a href='.AT_BASE_HREF.url_rewrite('content.php?cid='.$row['content_id']). '>' . $contentManager->_menu_info[$row['content_id']]['title'] . '</a></td>';
-			echo '<td>' . $row['total_hits'] . '</td>';
-			echo '<td>' . $row['total_duration'] . '</td>';
-			if ($row['last_accessed'] == '') {
-				echo '<td>' . _AT('na') . '</td>';
-			} else {
-				echo '<td>' . AT_date(_AT('forum_date_format'), $row['last_accessed'], AT_DATE_MYSQL_DATETIME) . '</td>';
-			}
-			echo '</tr>';
-		} //end while
-
-		echo '</tbody>';
-
-	} else {
-	    if($_SESSION['is_admin'] == 1){
-            $msg->printInfos('TRACKING_NO');
-        } 
-		echo '<tr><td colspan="4">' . _AT('none_found') . '</td></tr>';
-		echo '</tbody>';
-
-	}
-	?>
-</tbody>
-</table>
-
-
-<span id = "bargraph"> 
-
-</span>
+<span id = "bargraph">  </span>
 
 <table class="data static" >
 <thead>
@@ -114,9 +56,8 @@ require('./css/student_bar_graph.css');
 	$rows_hits = array_merge($rows_hits1,$rows_hits2);
     if(count($rows_hits) > 0){
 	    foreach($rows_hits as $row){
-			
 			echo '<tr>';
-			echo '<td>' .  $row['tool'] . '</td>';
+			echo '<td><a href='.AT_BASE_HREF.url_rewrite('mods/_standard/tracker/'.strtolower($row['tool']).'_details.php').'>' . $row['tool'] . '</a> </td>';
 			echo '<td>' . gmdate('H:i:s', (int)$row['Avg_time']) . '</td>';
 			echo '<td>' . gmdate('H:i:s', (int)$row['Your_avg_time']) . '</td>';
 			
