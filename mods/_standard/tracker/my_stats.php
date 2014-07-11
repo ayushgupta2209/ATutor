2afe6d76a10b315c49608e5f11bd86b4d99b428b
AYUSH GUPTA <ayushguptadtu@gmail.com>
2014-07-12 00:09:59 +0530

    proper indentation of mods/_standard/tracker/my_stats.php

--- mods/_standard/tracker/my_stats.php
+++ mods/_standard/tracker/my_stats.php
@@ -16,55 +16,53 @@ define('AT_INCLUDE_PATH', '../../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 require(AT_INCLUDE_PATH.'header.inc.php');
 require('./css/student_bar_graph.css');
-
 ?>
 <span id = "bargraph">  </span>
 
 <table class="data static" >
-<thead>
-<tr>
-	<th scope="col"><?php echo 'Tool'; ?></th>
-	<th scope="col"><?php echo 'Students_avg_time'; ?></th>
-	<th scope="col"><?php echo 'Your_avg_time'; ?></th>
-</tr>
-</thead>
-<?php
-
-	$sql1 = "SELECT a.tool, a.Avg_time, b.Your_avg_time FROM
-					(SELECT `tool_name` as tool, SUM(`duration`)/SUM(`counter`) as Avg_time 
-							FROM %stool_track 
-							WHERE `course_id` = %d 
-							GROUP BY `tool_name`)a
-					JOIN (SELECT `tool_name` as tool, SUM(`duration`)/SUM(`counter`) as Your_avg_time 
-				FROM %stool_track 
-				WHERE `member_id` = %d AND `course_id` = %d
-				GROUP BY `tool_name`)b
-				ON a.tool = b.tool";
-	
-	$sql2 =	"SELECT 'CONTENT' as tool, a.Avg_time, b.Your_avg_time FROM
-					( SELECT SUM(`duration`)/SUM(`counter`) as Avg_time, `course_id`
-							FROM %smember_track 
-							WHERE `course_id` = %d )a
-					JOIN (SELECT SUM(`duration`)/SUM(`counter`) as Your_avg_time,
-							`course_id`
-				FROM %smember_track 
-				WHERE `member_id` = %d AND `course_id` = %d )b
-				ON a.course_id = b.course_id";
+	<thead>
+		<tr>
+			<th scope="col"><?php echo 'Tool'; ?></th>
+			<th scope="col"><?php echo 'Students_avg_time'; ?></th>
+			<th scope="col"><?php echo 'Your_avg_time'; ?></th>
+		</tr>
+	</thead>
+	<?php
+		$sql1 = "SELECT a.tool, a.Avg_time, b.Your_avg_time FROM
+						(SELECT `tool_name` as tool, SUM(`duration`)/SUM(`counter`) as Avg_time 
+								FROM %stool_track 
+								WHERE `course_id` = %d 
+								GROUP BY `tool_name`)a
+						JOIN (SELECT `tool_name` as tool, SUM(`duration`)/SUM(`counter`) as Your_avg_time 
+					FROM %stool_track 
+					WHERE `member_id` = %d AND `course_id` = %d
+					GROUP BY `tool_name`)b
+					ON a.tool = b.tool";
+		
+		$sql2 =	"SELECT 'CONTENT' as tool, a.Avg_time, b.Your_avg_time FROM
+						( SELECT SUM(`duration`)/SUM(`counter`) as Avg_time, `course_id`
+								FROM %smember_track 
+								WHERE `course_id` = %d )a
+						JOIN (SELECT SUM(`duration`)/SUM(`counter`) as Your_avg_time,
+								`course_id`
+					FROM %smember_track 
+					WHERE `member_id` = %d AND `course_id` = %d )b
+					ON a.course_id = b.course_id";
+					
+		$rows_hits1 = queryDB($sql1, array(TABLE_PREFIX, $_SESSION['course_id'], TABLE_PREFIX, $_SESSION['member_id'], $_SESSION['course_id']));
+		$rows_hits2 = queryDB($sql2, array(TABLE_PREFIX, $_SESSION['course_id'], TABLE_PREFIX, $_SESSION['member_id'], $_SESSION['course_id']));
+		$rows_hits = array_merge($rows_hits1,$rows_hits2);
+		if(count($rows_hits) > 0){
+			foreach($rows_hits as $row){
+				echo '<tr>';
+				echo '<td><a href='.AT_BASE_HREF.url_rewrite('mods/_standard/tracker/'.strtolower($row['tool']).'_details.php').'>' . $row['tool'] . '</a> </td>';
+				echo '<td>' . gmdate('H:i:s', (int)$row['Avg_time']) . '</td>';
+				echo '<td>' . gmdate('H:i:s', (int)$row['Your_avg_time']) . '</td>';
 				
-	$rows_hits1 = queryDB($sql1, array(TABLE_PREFIX, $_SESSION['course_id'], TABLE_PREFIX, $_SESSION['member_id'], $_SESSION['course_id']));
-	$rows_hits2 = queryDB($sql2, array(TABLE_PREFIX, $_SESSION['course_id'], TABLE_PREFIX, $_SESSION['member_id'], $_SESSION['course_id']));
-	$rows_hits = array_merge($rows_hits1,$rows_hits2);
-    if(count($rows_hits) > 0){
-	    foreach($rows_hits as $row){
-			echo '<tr>';
-			echo '<td><a href='.AT_BASE_HREF.url_rewrite('mods/_standard/tracker/'.strtolower($row['tool']).'_details.php').'>' . $row['tool'] . '</a> </td>';
-			echo '<td>' . gmdate('H:i:s', (int)$row['Avg_time']) . '</td>';
-			echo '<td>' . gmdate('H:i:s', (int)$row['Your_avg_time']) . '</td>';
-			
-			echo '</tr>';
-		} //end while
-		echo '</tbody>';
-	}
+				echo '</tr>';
+			} //end while
+			echo '</tbody>';
+		}
 	?>
 </table>
 </body>
