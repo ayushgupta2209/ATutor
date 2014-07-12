be23820db9dc0ff2d5a9d45ee2695b75f693dc16
AYUSH GUPTA <ayushguptadtu@gmail.com>
2014-07-12 22:32:23 +0530

    blogs_details.php added with table and pie chart

--- mods/_standard/tracker/blogs_details.php
+++ mods/_standard/tracker/blogs_details.php
@@ -16,3 +16,62 @@ define('AT_INCLUDE_PATH', '../../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 require(AT_INCLUDE_PATH.'header.inc.php');
 ?>
+
+<div id="piechart" align= "center"></div>
+<table class="data static" summary="">
+	<thead>
+		<tr>
+			<th scope="col"><?php echo "Blog"; ?></th>
+			<th scope="col"><?php echo "Average Duration"; ?></th>
+			<th scope="col"><?php echo _AT('duration'); ?></th>
+			<th scope="col"><?php echo _AT('last_accessed'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			$sql = "SELECT a.main_tool_id, a.sub_tool_id as tool_id, b.title, a.average_duration, a.total_duration, a.last_accessed FROM
+						(SELECT main_tool_id, sub_tool_id, SEC_TO_TIME(SUM(`duration`)/SUM(`counter`)) as average_duration, 
+						SEC_TO_TIME(SUM(`duration`)) as total_duration, last_accessed
+								FROM %stool_track 
+								WHERE `member_id` = %d AND `course_id` = %d AND `tool_name` = '%s'
+								GROUP BY `sub_tool_id`)a
+						JOIN (SELECT `post_id`, `title` 
+					FROM %sblog_posts
+					WHERE 1)b
+					ON a.sub_tool_id = b.post_id";
+
+			$rows_hits = queryDB($sql, array(TABLE_PREFIX, $_SESSION['member_id'], $_SESSION['course_id'], "BLOGS", TABLE_PREFIX));
+			if(count($rows_hits) > 0) {
+				foreach($rows_hits as &$row){
+					echo '<tr>';
+					echo '<td><a href='.AT_BASE_HREF.url_rewrite('mods/_standard/blogs/post.php?ot=1&oid='.$row['main_tool_id'].'&id='.$row['tool_id']).'>' . $row['title']. '</a></td>';
+					echo '<td>' . $row['average_duration'] . '</td>';
+					echo '<td>' . $row['total_duration'] . '</td>';
+					if ($row['last_accessed'] == '') {
+						echo '<td>' . _AT('na') . '</td>';
+					} 
+					else {
+						echo '<td>' . AT_date(_AT('forum_date_format'), $row['last_accessed'], AT_DATE_MYSQL_DATETIME) . '</td>';
+					}
+					echo '</tr>';
+					$row['tool_id'] = $row['title'];
+				} //end foreach
+				echo '</tbody>';
+			}
+			else {
+				if($_SESSION['is_admin'] == 1){
+					$msg->printInfos('TRACKING_NO');
+				}
+				echo '<tr><td colspan="4">' . _AT('none_found') . '</td></tr>';
+				echo '</tbody>';
+			}
+		?>
+	</tbody>
+</table>
+<?php require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
+<script>
+	<?php	
+		require('../../../jscripts/d3js/d3.v3.min.js');
+		require('js/content_pie_chart.js');
+	?>
+</script>
